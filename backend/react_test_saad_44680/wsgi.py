0e5ecdf8c42d496565b1cc32f3c76e5d98457227
Crowdbotics <team@crowdbotics.com>
2023-11-20 07:05:26 +0000

    Initial commit

---
+++ backend/react_test_saad_44680/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for react_test_saad_44680 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "react_test_saad_44680.settings"
+)
+
+application = get_wsgi_application()
